4c8f99cba6e85f2fee411a5a0bafbdf7927fe07d
xshaobaozi <xshaobaozi@hotmail.com>
2019-10-21 00:20:37 +0800

    Mod: test

--- src/layout/head/index.js
+++ src/layout/head/index.js
@@ -4,22 +4,35 @@ import { Input } from 'antd';
 import {
     Link
 } from 'react-router-dom';
-
+import { withRouter } from "react-router-dom";
 const { Search } = Input;
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
     handleSearch(val) {
-        console.log(val)
+        // this.props.history.push('/detail')
+        this.props.history.push({
+            pathname: '/detail', 
+            query: {
+                q: 21312
+            }
+        })
+
     }
     render() {
         return (
             <div className="head-app">
                 <Link className="head-title" to="/home">小甲虫</Link><br />
                 <div className="head-search">
-                    <Search placeholder="请输入名称或链接" onSearch={this.handleSearch} style={{ width: 300 }} />
+                    <Search placeholder="请输入名称或链接" onSearch={this.handleSearch.bind(this)} style={{ width: 300 }} />
                 </div>
             </div>
         );
     }
 }
 
-export default App;
+export default withRouter(App);
